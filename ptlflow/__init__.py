25c19a1c0160edd52cdd753fac4682aa26284484
Henrique Morimitsu <henriquem87@gmail.com>
2021-06-27 00:08:49 +0800

    Update version to 0.1.6

--- ptlflow/__init__.py
+++ ptlflow/__init__.py
@@ -16,7 +16,7 @@
 # limitations under the License.
 # =============================================================================
 
-__version__ = '0.1.5'
+__version__ = '0.1.6'
 
 import logging
 from argparse import Namespace
