1d512112801d7a58d1a770e5f77a469ff2d8b334
Henrique Morimitsu <henriquem87@gmail.com>
2021-09-12 17:18:41 +0800

    Added GMA model

--- ptlflow/__init__.py
+++ ptlflow/__init__.py
@@ -35,6 +35,7 @@ from ptlflow.models.flownet.flownetcs import FlowNetCS
 from ptlflow.models.flownet.flownetcss import FlowNetCSS
 from ptlflow.models.flownet.flownets import FlowNetS
 from ptlflow.models.flownet.flownetsd import FlowNetSD
+from ptlflow.models.gma.gma import GMA
 from ptlflow.models.hd3.hd3 import HD3, HD3Context
 from ptlflow.models.irr.pwcnet import IRRPWCNet
 from ptlflow.models.irr.pwcnet_irr import IRRPWCNetIRR
@@ -63,6 +64,7 @@ models_dict = {
     'flownetcss': FlowNetCSS,
     'flownets': FlowNetS,
     'flownetsd': FlowNetSD,
+    'gma': GMA,
     'hd3': HD3,
     'hd3_ctxt': HD3Context,
     'irr_pwc': IRRPWC,
