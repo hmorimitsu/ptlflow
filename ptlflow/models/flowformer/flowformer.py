4f9ee16d21499b408d315fab1ce957a19a65909d
Henrique Morimitsu <henriquem87@gmail.com>
2024-02-23 20:43:28 +0800

    Black formatting

--- ptlflow/models/flowformer/flowformer.py
+++ ptlflow/models/flowformer/flowformer.py
@@ -178,7 +178,9 @@ class FlowFormer(BaseModel):
         image_size = (max(self.args.tile_height, input_size[-2]), input_size[-1])
         hws = compute_grid_indices(image_size, train_size)
         device = inputs["images"].device
-        weights = compute_weight(hws, image_size, train_size, self.args.tile_sigma, device=device)
+        weights = compute_weight(
+            hws, image_size, train_size, self.args.tile_sigma, device=device
+        )
 
         images, image_resizer = self.preprocess_images(
             inputs["images"],
