1696c9eb31d0f056931f0f3b6170d2742f8ed205
Henrique Morimitsu <henriquem87@gmail.com>
2025-03-20 16:30:22 +0800

    Add paper results comparisons for dpflow and streamflow

---
+++ misc/create_compare_paper.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python ../compare_paper_results.py --paper_results_path ../docs/source/results/paper_results_things.csv --validate_results_path ../docs/source/results/metrics_all_things.csv --add_delta
