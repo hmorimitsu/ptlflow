52e3b7f1352b41131dbd7bd47dfff977621190d9
Henrique Morimitsu <henriquem87@gmail.com>
2026-03-28 00:19:08 +0800

    Fix stride in test_models

--- tests/ptlflow/models/test_models.py
+++ tests/ptlflow/models/test_models.py
@@ -91,10 +91,11 @@ def test_forward() -> None:
         model = ptlflow.get_model(mname, args=args)
         model = model.eval()
 
+        s = make_divisible(256, model.output_stride)
         num_images = 2
         if mname in ["videoflow_bof", "videoflow_mof"]:
             num_images = 3
-        inputs = {"images": torch.rand(1, num_images, 3, 256, 256)}
+        inputs = {"images": torch.rand(1, num_images, 3, s, s)}
 
         if torch.cuda.is_available():
             model = model.cuda()
@@ -124,10 +125,11 @@ def test_forward_fp16() -> None:
             model = model.eval()
             model = model.half()
 
+            s = make_divisible(256, model.output_stride)
             num_images = 2
             if mname in ["videoflow_bof", "videoflow_mof"]:
                 num_images = 3
-            inputs = {"images": torch.rand(1, num_images, 3, 256, 256)}
+            inputs = {"images": torch.rand(1, num_images, 3, s, s)}
 
             if torch.cuda.is_available():
                 model = model.cuda()
